5ab1ef4cebdd39b3ecfa49b000821e56467950ef
Daniel Bachler <daniel@danielbachler.de>
2015-12-07 18:56:21 +0100

    Removed dependency on FileReader by letting the user pass in the decode function into dropZoneEventHandlers

--- src/DropZone.elm
+++ src/DropZone.elm
@@ -28,8 +28,6 @@ import Html.Events exposing (onWithOptions)
 import Json.Decode as Json exposing (andThen)
 import Effects exposing (Effects)
 
-import FileReader exposing (parseDroppedFiles, NativeFile)
-
 -- MODEL
 
 {-| Indicates if the user is hovering over the element. Can be used to change the 
@@ -84,44 +82,52 @@ init = { hoverState = Normal }
 the user dropped onto the element. Handle this action in your code and do something
 with the files.
 -}
-type Action
+type Action a
     = DragEnter -- user enters the drop zone while dragging something
     | DragLeave -- user leaves drop zone
-    | Drop (List NativeFile)
+    | Drop a
 
 {-| Updates the Model from an Action.
 -}
-update : Action -> Model -> Model
+update : Action a -> Model -> Model
 update action model =
     case action of
         DragEnter ->
             {model | hoverState = Hovering }
         DragLeave ->
             {model | hoverState = Normal }
-        Drop files ->
+        Drop a ->
             {model | hoverState = Normal }
 
 {-| Returns a list of Attributes to add to an element to turn it into a 
-"Drpozone" by registering the required event handlers.
+"Dropzone" by registering the required event handlers. 
+
+The Json.Decoder you pass in is used to extract the data from the drop operation.
+If the drop operation is a user dropping files in a browser, you will want to 
+extract the .dataTransfer.files content. The FileReader project
+( https://github.com/simonh1000/file-reader ) provides a convinience function 
+`parseDroppedFiles` to do this for you.
 
     -- Example view, renders a div that acts as a dropzone by 
     -- adding the dragDropEventHandlers. Note that DropZoneAction 
     -- would be one of your components Actions, tagged with 
-    -- the Action of the Dropzone
+    -- the Action of the Dropzone. Since we pass just a dummy Json.value
+    -- decoder, the Drop action will be tagged with this. A real example
+    -- may want to use parseDroppedFiles
     view : Signal.Address Action -> Model -> Html
     view address model =
         div
         (  dropZoneStyle model.dropZoneModel 
-        :: dragDropEventHandlers (Signal.forwardTo address DropZoneAction))    
+        :: dragDropEventHandlers (Json.value) (Signal.forwardTo address DropZoneAction))    
         [ renderImageOrPrompt model
         ]
 -}
-dropZoneEventHandlers : Signal.Address Action -> List Attribute
-dropZoneEventHandlers address =
+dropZoneEventHandlers : (Json.Decoder a) -> Signal.Address (Action a) -> List Attribute
+dropZoneEventHandlers decoder address =
     [ onDragEnter address DragEnter
     , onDragLeave address DragLeave
     , onDragOver address DragEnter
-    , onDrop address
+    , onDrop address decoder
     ]
 
 -- Individual handler functions
@@ -133,12 +139,12 @@ onDragFunctionIgnoreFiles nativeEventName address action =
         Json.value
         (\_ -> Signal.message address action)
 
-onDragFunctionDecodeFiles : String -> (List NativeFile -> Action) -> Signal.Address Action -> Attribute
-onDragFunctionDecodeFiles nativeEventName actionCreator address =
+onDragFunctionDecodeFiles : String -> (Json.Decoder a) -> (a -> (Action a)) -> Signal.Address (Action a) -> Attribute
+onDragFunctionDecodeFiles nativeEventName decoder actionCreator address =
     onWithOptions
         nativeEventName
         {stopPropagation = True, preventDefault = True}
-        parseDroppedFiles
+        decoder
         (\vals -> Signal.message address (actionCreator vals))
 
 onDragEnter : Signal.Address a -> a -> Attribute
@@ -153,6 +159,6 @@ onDragLeave : Signal.Address a -> a -> Attribute
 onDragLeave =
   onDragFunctionIgnoreFiles "dragleave"
 
-onDrop : Signal.Address Action -> Attribute
-onDrop address =
-  onDragFunctionDecodeFiles "drop" (\files -> Drop files) address
+onDrop : Signal.Address (Action a) -> (Json.Decoder a) -> Attribute
+onDrop address decoder =
+  onDragFunctionDecodeFiles "drop" decoder (\dropContent -> Drop dropContent) address
