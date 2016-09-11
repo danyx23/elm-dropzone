3a3b0a3e39d7bd05a4da6e98789d84e9fc97a86a
Daniel Bachler <daniel@danielbachler.de>
2016-09-11 15:41:41 +0200

    Upgraded to 0.17

--- examples/ReadDroppedFilesAsText.elm
+++ examples/ReadDroppedFilesAsText.elm
@@ -1,154 +1,191 @@
+module Main exposing (..)
+
 import Html exposing (Html, div, input, button, h1, p, text)
 import Html.Attributes exposing (type', id, style)
-import Html.Events exposing (onClick, on)
-import StartApp
-import Effects exposing (Effects)
+import Html.App
 import Task
-import Json.Decode as Json exposing (Value, andThen)
 import FileReader exposing (FileRef, NativeFile, readAsTextFile, Error(..))
-import DropZone exposing (Action(Drop), dropZoneEventHandlers, isHovering)
-import MimeType exposing (MimeType(Text))
+import DropZone exposing (DropZoneMessage(Drop), dropZoneEventHandlers, isHovering)
+
 
 {- Attention! For this example to work you need to have a copy of simonh1000/filereader
-in this directory. Since filereader has not passed native review as of writing this,
-you need to directly clone the filereader repository into this directory. The elm-package.json
-is already configured to look for the source of filereader inside this directory.
+   in this directory. Since filereader has not passed native review as of writing this,
+   you need to directly clone the filereader repository into this directory. The elm-package.json
+   is already configured to look for the source of filereader inside this directory.
 
-To clone, please run this command from inside the examples directory:
-git clone https://github.com/simonh1000/file-reader
+   To clone, please run this command from inside the examples directory:
+   git clone https://github.com/simonh1000/file-reader
 
-This example was adapted from Simon Hamptons example in the file-reader library.
+   This example was adapted from Simon Hamptons example in the file-reader library.
 -}
-
-
 -- MODEL
+
 
 type alias Model =
     { message : String
-    , dropZone : DropZone.Model -- store the DropZone model in your apps Model
+    , dropZone :
+        DropZone.Model
+        -- store the DropZone model in your apps Model
     , files : List NativeFile
     , contents : List String
     }
 
+
 init : Model
 init =
     { message = "Waiting..."
-    , dropZone = DropZone.init -- call DropZone.init to initialize
+    , dropZone =
+        DropZone.init
+        -- call DropZone.init to initialize
     , files = []
     , contents = []
     }
 
+
+
 -- UPDATE
 
-type Action
-    = DnD (DropZone.Action (List NativeFile)) -- add an Action that takes care of hovering, dropping etc
-    | FileData (Result FileReader.Error String)
 
-update : Action -> Model -> (Model, Effects Action)
+type Action
+    = DnD (DropZone.DropZoneMessage (List NativeFile))
+      -- add an Action that takes care of hovering, dropping etc
+    | FileReadSucceeded ( String, String )
+    | FileReadFailed FileReader.Error
+
+
+update : Action -> Model -> ( Model, Cmd Action )
 update action model =
     case action of
-        DnD (Drop files) -> -- this happens when the user dropped something into the dropzone
+        DnD (Drop files) ->
+            -- this happens when the user dropped something into the dropzone
             ( { model
-              | dropZone = DropZone.update (Drop files) model.dropZone -- update the DropZone model 
-              , files = files -- and store the dropped files
+                | dropZone =
+                    DropZone.update (Drop files) model.dropZone
+                    -- update the DropZone model
+                , files =
+                    files
+                    -- and store the dropped files
               }
-            , Effects.batch <| -- also create a bunch of effects to read the files as text, one effect for each file
-                List.map (readTextFile << .blob) files
+            , Cmd.batch
+                <| -- also create a bunch of effects to read the files as text, one effect for each file
+                   List.map (readTextFile << .blob) files
             )
-        DnD a -> -- these are opaque DropZone actions, just hand them to DropZone to deal with them
+
+        DnD a ->
+            -- these are opaque DropZone actions, just hand them to DropZone to deal with them
             ( { model | dropZone = DropZone.update a model.dropZone }
-            , Effects.none
+            , Cmd.none
             )
 
-        FileData (Result.Ok str) -> -- this happens when an effect has finished and the file has successfully been loaded
-            ( { model 
-                  | contents = str :: model.contents
-                  , message = "Successfully loaded at least one file" 
+        FileReadSucceeded ( modificationDate, str ) ->
+            -- this happens when an effect has finished and the file has successfully been loaded
+            ( { model
+                | contents = str :: model.contents
+                , message = "Successfully loaded at least one file"
               }
-            , Effects.none
+            , Cmd.none
             )
 
-        FileData (Result.Err err) -> -- this happens when an effect has finished and there was an error loading hte file
+        FileReadFailed err ->
+            -- this happens when an effect has finished and there was an error loading hte file
             ( { model | message = FileReader.toString err }
-            , Effects.none
+            , Cmd.none
             )
+
+
 
 -- VIEW
 
-view : Signal.Address Action -> Model -> Html
-view address model =
+
+view : Model -> Html Action
+view model =
     div [ containerStyles ]
         [ h1 [] [ text "Drag 'n Drop" ]
-        , renderDropZone address (model.dropZone) -- render the dropzone
-        , div
-            [] -- list the files that were dropped
+        , renderDropZone (model.dropZone)
+          -- render the dropzone
+        , div []
+            -- list the files that were dropped
             [ text <| "Files: " ++ commaSeparate (List.map .name model.files)
             ]
-        , div
-            [] <| -- and the contents
-            [ text <| "Content: " ++ commaSeparate model.contents ]
+        , div []
+            <| -- and the contents
+               [ text <| "Content: " ++ commaSeparate model.contents ]
         , p [] [ text model.message ]
         ]
+
 
 commaSeparate : List String -> String
 commaSeparate lst =
     List.foldl (++) "" (List.intersperse ", " lst)
 
-renderDropZone :  Signal.Address Action -> DropZone.Model -> Html
-renderDropZone address dropZoneModel =
-  div
-    (renderZoneAttributes address dropZoneModel)
-    []
 
-renderZoneAttributes :  Signal.Address Action -> DropZone.Model -> List Html.Attribute
-renderZoneAttributes address dropZoneModel =
-    ( if DropZone.isHovering dropZoneModel then
-        dropZoneHover -- style the dropzone differently depending on whether the user is hovering
-      else
-        dropZoneDefault          
+renderDropZone : DropZone.Model -> Html Action
+renderDropZone dropZoneModel =
+    Html.App.map DnD
+        (div (renderZoneAttributes dropZoneModel) [])
+
+
+renderZoneAttributes :
+    DropZone.Model
+    -> List (Html.Attribute (DropZoneMessage (List NativeFile)))
+renderZoneAttributes dropZoneModel =
+    (if DropZone.isHovering dropZoneModel then
+        dropZoneHover
+        -- style the dropzone differently depending on whether the user is hovering
+     else
+        dropZoneDefault
     )
-    ::
-    -- add the necessary DropZone event wiring 
-    dropZoneEventHandlers FileReader.parseDroppedFiles (Signal.forwardTo address DnD)
+        :: -- add the necessary DropZone event wiring
+           dropZoneEventHandlers FileReader.parseDroppedFiles
 
+
+containerStyles : Html.Attribute a
 containerStyles =
-    style [ ( "padding", "20px") ]
-    
+    style [ ( "padding", "20px" ) ]
+
+
+dropZoneDefault : Html.Attribute a
 dropZoneDefault =
     style
-        [ ( "height", "120px")
-        , ( "border-radius", "10px")
-        , ( "border", "3px dashed steelblue")
+        [ ( "height", "120px" )
+        , ( "border-radius", "10px" )
+        , ( "border", "3px dashed steelblue" )
         ]
-        
+
+
+dropZoneHover : Html.Attribute a
 dropZoneHover =
     style
-        [ ( "height", "120px")
-        , ( "border-radius", "10px")
-        , ( "border", "3px dashed red")
+        [ ( "height", "120px" )
+        , ( "border-radius", "10px" )
+        , ( "border", "3px dashed red" )
         ]
+
+
 
 -- TASKS
 
-readTextFile : FileRef -> Effects Action
+
+readTextFile : FileRef -> Cmd Action
 readTextFile fileValue =
     readAsTextFile fileValue
-        |> Task.toResult
-        |> Task.map FileData
-        |> Effects.task
+        |> Task.perform FileReadFailed FileReadSucceeded
+
+
 
 -- ----------------------------------
+
+
+app : Program Never
 app =
-    StartApp.start
-        { init = (init, Effects.none)
+    Html.App.program
+        { init = ( init, Cmd.none )
         , update = update
         , view = view
-        , inputs = []
+        , subscriptions = (\_ -> Sub.none)
         }
 
-main =
-    app.html
 
-port tasks : Signal (Task.Task Effects.Never ())
-port tasks =
-    app.tasks
+main : Program Never
+main =
+    app
--- src/DropZone.elm
+++ src/DropZone.elm
@@ -1,20 +1,21 @@
 module DropZone
-  ( Model
-  , Action(Drop)
-  , isHovering
-  , init
-  , update
-  , dropZoneEventHandlers
-  ) where
+    exposing
+        ( Model
+        , DropZoneMessage(Drop)
+        , isHovering
+        , init
+        , update
+        , dropZoneEventHandlers
+        )
 
 {-| This library makes it easier to use Html5 Drag/Drop events when you want
-to support dropping of files into a webpage. 
+to support dropping of files into a webpage.
 
 # Main DnD support
 @docs dropZoneEventHandlers
 
 # Drop action
-@docs Action
+@docs DropZoneMessage
 
 # Model
 @docs Model
@@ -25,12 +26,13 @@ to support dropping of files into a webpage.
 
 import Html exposing (Attribute)
 import Html.Events exposing (onWithOptions)
-import Json.Decode as Json exposing (andThen)
-import Effects exposing (Effects)
+import Json.Decode as Json exposing (andThen, map)
+
 
 -- MODEL
 
-{-| Indicates if the user is hovering over the element. Can be used to change the 
+
+{-| Indicates if the user is hovering over the element. Can be used to change the
 styling of the Dropzone when the user is dragging something on top of it. Possible
 states are Normal and Hovering.
 
@@ -44,121 +46,142 @@ type HoverState
     = Normal
     | Hovering
 
+
 {-| Opaque model of the DropZone.
 -}
-type alias Model = {
-  hoverState: HoverState -- set to Hovering if the user is hovering with content over the drop zone
-}
+type alias Model =
+    { hoverState :
+        HoverState
+        -- set to Hovering if the user is hovering with content over the drop zone
+    }
+
 
 {-| Function that tells you if the user is currently hovering over the dropzone
-with a Drag operation. 
+with a Drag operation.
 
 This information is stored inside the model and thus
 isHovering can only give you a correct information if you attached the event
-handlers to the dropzone you render and make sure that Dropzone Actions are "routed"
+handlers to the dropzone you render and make sure that Dropzone DropZoneMessages are "routed"
 to the update function of the DropZone
 
-    getDropZoneAttributes : Signal.Address Action -> DropZone.Model -> List Html.Attribute
-    getDropZoneAttributes address dropZoneModel =
+    getDropZoneAttributes : DropZone.Model -> List Html.Attribute msg
+    getDropZoneAttributes dropZoneModel =
         ( if (DropZone.isHovering dropZoneModel) then
             style [( "border", "3px dashed red")]
           else
             style [( "border", "3px dashed steelblue")]
         )
         ::
-        dragDropEventHandlers (Signal.forwardTo address DnD)
+        dragDropEventHandlers payloadDecoder)
 -}
 isHovering : Model -> Bool
 isHovering model =
-  model.hoverState == Hovering
+    model.hoverState == Hovering
+
 
 {-| Initializes the Model
 -}
 init : Model
-init = { hoverState = Normal }
+init =
+    { hoverState = Normal }
+
+
 
 -- UPDATE
-{-| The Drop actions is tagged with a (List NativeFile) that represent the files
+
+
+{-| The Drop actions is usually tagged with a (List NativeFile) that represent the files
 the user dropped onto the element. Handle this action in your code and do something
 with the files.
 -}
-type Action a
-    = DragEnter -- user enters the drop zone while dragging something
-    | DragLeave -- user leaves drop zone
+type DropZoneMessage a
+    = DragEnter
+      -- user enters the drop zone while dragging something
+    | DragLeave
+      -- user leaves drop zone
     | Drop a
 
-{-| Updates the Model from an Action.
+
+{-| Updates the Model from a DropZoneMessage.
 -}
-update : Action a -> Model -> Model
+update : DropZoneMessage a -> Model -> Model
 update action model =
     case action of
         DragEnter ->
-            {model | hoverState = Hovering }
-        DragLeave ->
-            {model | hoverState = Normal }
-        Drop a ->
-            {model | hoverState = Normal }
+            { model | hoverState = Hovering }
 
-{-| Returns a list of Attributes to add to an element to turn it into a 
-"Dropzone" by registering the required event handlers. 
+        DragLeave ->
+            { model | hoverState = Normal }
+
+        Drop a ->
+            { model | hoverState = Normal }
+
+
+{-| Returns a list of Attributes to add to an element to turn it into a
+"Dropzone" by registering the required event handlers.
 
 The Json.Decoder you pass in is used to extract the data from the drop operation.
-If the drop operation is a user dropping files in a browser, you will want to 
+If the drop operation is a user dropping files in a browser, you will want to
 extract the .dataTransfer.files content. The FileReader project
-( https://github.com/simonh1000/file-reader ) provides a convinience function 
+( https://github.com/simonh1000/file-reader ) provides a convinience function
 `parseDroppedFiles` to do this for you.
 
-    -- Example view, renders a div that acts as a dropzone by 
-    -- adding the dragDropEventHandlers. Note that DropZoneAction 
-    -- would be one of your components Actions, tagged with 
-    -- the Action of the Dropzone. Since we pass just a dummy Json.value
+    -- Example view, renders a div that acts as a dropzone by
+    -- adding the dragDropEventHandlers. Note that DropZoneMessage
+    -- would be one of your components Messages, tagged with
+    -- the Message of the Dropzone. Since we pass just a dummy Json.value
     -- decoder, the Drop action will be tagged with this. A real example
     -- may want to use parseDroppedFiles
-    view : Signal.Address Action -> Model -> Html
+    view : Message -> Model -> Html
     view address model =
         div
-        (  dropZoneStyle model.dropZoneModel 
-        :: dragDropEventHandlers (Json.value) (Signal.forwardTo address DropZoneAction))    
+        (  dropZoneStyle model.dropZoneModel
+        :: dragDropEventHandlers (Json.value) )
         [ renderImageOrPrompt model
         ]
 -}
-dropZoneEventHandlers : (Json.Decoder a) -> Signal.Address (Action a) -> List Attribute
-dropZoneEventHandlers decoder address =
-    [ onDragEnter address DragEnter
-    , onDragLeave address DragLeave
-    , onDragOver address DragEnter
-    , onDrop address decoder
+dropZoneEventHandlers : Json.Decoder a -> List (Attribute (DropZoneMessage a))
+dropZoneEventHandlers decoder =
+    [ onDragEnter DragEnter
+    , onDragLeave DragLeave
+    , onDragOver DragEnter
+    , onDrop decoder
     ]
 
+
+
 -- Individual handler functions
-onDragFunctionIgnoreFiles : String -> Signal.Address a -> a -> Attribute
-onDragFunctionIgnoreFiles nativeEventName address action =
-    onWithOptions
-        nativeEventName
-        {stopPropagation = False, preventDefault = True}
-        Json.value
-        (\_ -> Signal.message address action)
 
-onDragFunctionDecodeFiles : String -> (Json.Decoder a) -> (a -> (Action a)) -> Signal.Address (Action a) -> Attribute
-onDragFunctionDecodeFiles nativeEventName decoder actionCreator address =
-    onWithOptions
-        nativeEventName
-        {stopPropagation = True, preventDefault = True}
+
+onDragFunctionIgnoreFiles : String -> a -> Attribute a
+onDragFunctionIgnoreFiles nativeEventName action =
+    onWithOptions nativeEventName
+        { stopPropagation = False, preventDefault = True }
+        (Json.succeed action)
+
+
+onDragFunctionDecodeFiles : String -> Json.Decoder (DropZoneMessage a) -> Attribute (DropZoneMessage a)
+onDragFunctionDecodeFiles nativeEventName decoder =
+    onWithOptions nativeEventName
+        { stopPropagation = True, preventDefault = True }
         decoder
-        (\vals -> Signal.message address (actionCreator vals))
 
-onDragEnter : Signal.Address a -> a -> Attribute
+
+onDragEnter : a -> Attribute a
 onDragEnter =
-  onDragFunctionIgnoreFiles "dragenter"
+    onDragFunctionIgnoreFiles "dragenter"
 
-onDragOver : Signal.Address a -> a -> Attribute
+
+onDragOver : a -> Attribute a
 onDragOver =
-  onDragFunctionIgnoreFiles "dragover"
+    onDragFunctionIgnoreFiles "dragover"
 
-onDragLeave : Signal.Address a -> a -> Attribute
+
+onDragLeave : a -> Attribute a
 onDragLeave =
-  onDragFunctionIgnoreFiles "dragleave"
+    onDragFunctionIgnoreFiles "dragleave"
 
-onDrop : Signal.Address (Action a) -> (Json.Decoder a) -> Attribute
-onDrop address decoder =
-  onDragFunctionDecodeFiles "drop" decoder (\dropContent -> Drop dropContent) address
+
+onDrop : Json.Decoder a -> Attribute (DropZoneMessage a)
+onDrop decoder =
+    onDragFunctionDecodeFiles "drop" (map (\userVal -> Drop userVal) decoder)
